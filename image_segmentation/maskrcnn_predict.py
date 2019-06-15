21a731b72cbcbfa4182047c717cd538c95377485
aldo-sollazzo <aldo@noumena.io>
2019-06-15 17:47:40 +0200

    segm_02

--- image_segmentation/maskrcnn_predict.py
+++ image_segmentation/maskrcnn_predict.py
@@ -27,3 +27,28 @@ hsv = [(i / len(CLASS_NAMES), 1, 1.0) for i in range(len(CLASS_NAMES))]
 COLORS = list(map(lambda c: colorsys.hsv_to_rgb(*c), hsv))
 random.seed(42)
 random.shuffle(COLORS)
+
+class SimpleConfig(Config):
+    # give the configuration a recognizable name
+    NAME = "coco_inference"
+
+    # set the number of GPUs to use along with the number of images
+    # per GPU
+    GPU_COUNT = 1
+    IMAGES_PER_GPU = 1
+
+    # number of classes (we would normally add +1 for the background
+    # but the background class is *already* included in the class
+    # names)
+    NUM_CLASSES = len(CLASS_NAMES)
+
+
+# initialize the inference configuration
+config = SimpleConfig()
+
+# initialize the Mask R-CNN model for inference and then load the
+# weights
+print("[INFO] loading Mask R-CNN model...")
+model = modellib.MaskRCNN(mode="inference", config=config,
+                          model_dir=os.getcwd())
+model.load_weights(args["weights"], by_name=True)
