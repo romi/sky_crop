dad6cf554b794c86601c27a1bde02dae38b6368e
aldo-sollazzo <aldo@noumena.io>
2019-10-12 17:40:30 +0200

    -requirments.txt-

--- mask_rcnn/lettuce.py
+++ mask_rcnn/lettuce.py
@@ -248,51 +248,61 @@ if __name__ == "__main__":
             else model.find_last()
         model.load_weights(weights, by_name=True)
 
+        #define path
+        filepath = "/Users/aldo/Desktop/git-romi/sky_crop/mask_rcnn/examples/*"
+        savepath = "/Users/aldo/Desktop/git-romi/sky_crop/mask_rcnn/detection/"
 
-        # load the input image, convert it from BGR to RGB channel
-        # ordering, and resize the image
-        image = cv2.imread(args["image"])
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        image = imutils.resize(image, width=1024)
+        #start loop to read images
+        for imagepath in glob.glob(filepath):
 
-        # perform a forward pass of the network to obtain the results
-        r = model.detect([image], verbose=1)[0]
+            #get image name
+            filename = os.path.basename(imagepath)
 
-        # loop over of the detected object's bounding boxes and
-        # masks, drawing each as we go along
-        for i in range(0, r["rois"].shape[0]):
-            mask = r["masks"][:, :, i]
-            image = visualize.apply_mask(image, mask,
-                                         (1.0, 0.0, 0.0), alpha=0.5)
-            image = visualize.draw_box(image, r["rois"][i],
-                                       (1.0, 0.0, 0.0))
+            # load the input image, convert it from BGR to RGB channel
+            # ordering, and resize the image
+            image = cv2.imread(imagepath)
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+            image = imutils.resize(image, width=1024)
 
-        # convert the image back to BGR so we can use OpenCV's
-        # drawing functions
-        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+            # perform a forward pass of the network to obtain the results
+            r = model.detect([image], verbose=1)[0]
 
-        # loop over the predicted scores and class labels
-        for i in range(0, len(r["scores"])):
-            # extract the bounding box information, class ID, label,
-            # and predicted probability from the results
-            (startY, startX, endY, end) = r["rois"][i]
-            classID = r["class_ids"][i]
-            label = CLASS_NAMES[classID]
-            score = r["scores"][i]
+            # loop over of the detected object's bounding boxes and
+            # masks, drawing each as we go along
+            for i in range(0, r["rois"].shape[0]):
+                mask = r["masks"][:, :, i]
+                image = visualize.apply_mask(image, mask,
+                                             (1.0, 0.0, 0.0), alpha=0.5)
+                image = visualize.draw_box(image, r["rois"][i],
+                                           (1.0, 0.0, 0.0))
 
-            # draw the class label and score on the image
-            text = "{}: {:.4f}".format(label, score)
-            y = startY - 10 if startY - 10 > 10 else startY + 10
-            cv2.putText(image, text, (startX, y),
-                        cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 0), 2)
+            # convert the image back to BGR so we can use OpenCV's
+            # drawing functions
+            image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
 
-        # resize the image so it more easily fits on our screen
-        image = imutils.resize(image, width=512)
+            # loop over the predicted scores and class labels
+            for i in range(0, len(r["scores"])):
+                # extract the bounding box information, class ID, label,
+                # and predicted probability from the results
+                (startY, startX, endY, end) = r["rois"][i]
+                classID = r["class_ids"][i]
+                label = CLASS_NAMES[classID]
+                score = r["scores"][i]
 
-        # show the output image
-        cv2.imshow("Output", image)
-        cv2.imwrite("predict/LETTUCE_PREDICT.jpg", image)
-        cv2.waitKey(0)
+                # draw the class label and score on the image
+                text = "{}: {:.4f}".format(label, score)
+                y = startY - 10 if startY - 10 > 10 else startY + 10
+                cv2.putText(image, text, (startX, y),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 0), 2)
+
+            # resize the image so it more easily fits on our screen
+            image = imutils.resize(image, width=512)
+
+            # show and save the output image
+            imgDetect = os.path.join(savepath, "PREDICT_" + filename)
+            #cv2.imshow(filename, image)
+            cv2.imwrite(imgDetect, image)
+            #cv2.waitKey(0)
 
     # check to see if we are investigating our images and masks
     elif args["mode"] == "investigate":
