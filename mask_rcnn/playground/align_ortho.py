4d17854e30953a2c670eb8bbd32cde7cc87cd49c
aldo-sollazzo <aldo@noumena.io>
2020-03-11 15:16:46 +0100

    -pca pyplot-

--- mask_rcnn/playground/align_ortho.py
+++ mask_rcnn/playground/align_ortho.py
@@ -1,7 +1,8 @@
 import cv2
 import numpy as np
 from sklearn import decomposition
-from matplotlib import pyplot as plt
+import matplotlib.pyplot as plt
+import seaborn as sns; sns.set()
 
 # OPEN IMAGE AND EXTRACT ARRAY
 im=cv2.imread("/Users/aldo/Desktop/odm_orthophoto.tif",0)
@@ -9,39 +10,57 @@ idxs=np.array(np.where(im)).T
 
 # PRINCIPAL COMPONENT ANALYSIS
 p=decomposition.PCA()
-print(p)
 res=p.fit(idxs)
-tidxs=res.transform(idxs)
-L=np.int(tidxs[:,0].max()-tidxs[:,0].min())
-H=np.int(tidxs[:,1].max()-tidxs[:,1].min())
-angle=np.arctan(res.components_[0][0]/res.components_[0][1])
-print(angle)
-
+print(p.components_)
+print(p.explained_variance_)
 
 # VISUALIZE PCA
-fig = plt.figure(figsize = (8,8))
-ax = fig.add_subplot(1,1,1)
-ax.set_xlabel('Principal Component 1', fontsize = 15)
+def draw_vector(v0, v1, ax=None):
+    ax = ax or plt.gca()
+    arrowprops=dict(arrowstyle='->',
+                    linewidth=2,
+                    shrinkA=0, shrinkB=0)
+    ax.annotate('', v1, v0, arrowprops=arrowprops)
 
-targets = ['Iris-setosa']
-colors = ['r']
+# PLOT DATA
+plt.scatter(idxs[:, 0], idxs[:, 1], alpha=0.2)
+for length, vector in zip(p.explained_variance_, p.components_):
+    v = vector * 3 * np.sqrt(length)
+    draw_vector(p.mean_, p.mean_ + v)
+plt.axis('equal')
+plt.show()
 
-indicesToKeep = p['target'] == target
-ax.scatter(finalDf.loc[indicesToKeep, 'principal component 1']
-           , finalDf.loc[indicesToKeep, 'principal component 2']
-           , c = color
-           , s = 50)
-ax.legend(targets)
-ax.grid()
-
-# ROTATE IMAGE
-im=cv2.imread("/Users/aldo/Desktop/odm_orthophoto.tif")
-rot_mat = cv2.getRotationMatrix2D((im.shape[0],im.shape[1]), angle*70/np.pi, 1.0)
-# #rot_mat = cv2.getRotationMatrix2D((idxs[:,0].mean(),idxs[:,1].mean()), angle*180/np.pi, 1.0)
-result = cv2.warpAffine(im, rot_mat, (2*L,H), flags=cv2.INTER_LINEAR)
-# cv2.imshow('rotate',result)
-# cv2.waitKey(0)
-cv2.imwrite("rot-ortho.png", result)
+# tidxs=res.transform(idxs)
+# L=np.int(tidxs[:,0].max()-tidxs[:,0].min())
+# H=np.int(tidxs[:,1].max()-tidxs[:,1].min())
+# angle=np.arctan(res.components_[0][0]/res.components_[0][1])
+# print(angle)
+#
+#
+# # VISUALIZE PCA
+# fig = plt.figure(figsize = (8,8))
+# ax = fig.add_subplot(1,1,1)
+# ax.set_xlabel('Principal Component 1', fontsize = 15)
+#
+# targets = ['Iris-setosa']
+# colors = ['r']
+#
+# indicesToKeep = p['target'] == target
+# ax.scatter(finalDf.loc[indicesToKeep, 'principal component 1']
+#            , finalDf.loc[indicesToKeep, 'principal component 2']
+#            , c = color
+#            , s = 50)
+# ax.legend(targets)
+# ax.grid()
+#
+# # ROTATE IMAGE
+# im=cv2.imread("/Users/aldo/Desktop/odm_orthophoto.tif")
+# rot_mat = cv2.getRotationMatrix2D((im.shape[0],im.shape[1]), angle*70/np.pi, 1.0)
+# # #rot_mat = cv2.getRotationMatrix2D((idxs[:,0].mean(),idxs[:,1].mean()), angle*180/np.pi, 1.0)
+# result = cv2.warpAffine(im, rot_mat, (2*L,H), flags=cv2.INTER_LINEAR)
+# # cv2.imshow('rotate',result)
+# # cv2.waitKey(0)
+# cv2.imwrite("rot-ortho.png", result)
 
 # idxs=np.array(np.where(result[:,:,0])).T
 # print(idxs)
