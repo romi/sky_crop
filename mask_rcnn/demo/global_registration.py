241ffc00eab0baaaf96af04fe589de5c2b3725c2
coroush <soroush.garivani@iaac.net>
2020-03-24 19:56:44 +0100

    update

--- mask_rcnn/demo/global_registration.py
+++ mask_rcnn/demo/global_registration.py
@@ -24,11 +24,11 @@ def draw_registration_result(source, target, transformation):
 
 def preprocess_point_cloud(pcd, voxel_size):
     radius_normal = voxel_size * 2
-    print(":: Estimate normal with search radius %.3f." % radius_normal)
+    # print(":: Estimate normal with search radius %.3f." % radius_normal)
     pcd.estimate_normals(
         o3d.geometry.KDTreeSearchParamHybrid(radius=radius_normal, max_nn=30))
     radius_feature = voxel_size * 5
-    print(":: Compute FPFH feature with search radius %.3f." % radius_feature)
+    # print(":: Compute FPFH feature with search radius %.3f." % radius_feature)
     pcd_fpfh = o3d.registration.compute_fpfh_feature(
         pcd,
         o3d.geometry.KDTreeSearchParamHybrid(radius=radius_feature, max_nn=100))
@@ -36,7 +36,7 @@ def preprocess_point_cloud(pcd, voxel_size):
 
 
 def prepare_dataset(source, target, voxel_size):
-    print(":: Load two point clouds.")
+    # print(":: Load two point clouds.")
     # draw_registration_result(source, target, np.identity(4))
     source_fpfh = preprocess_point_cloud(source, voxel_size)
     target_fpfh = preprocess_point_cloud(target, voxel_size)
@@ -46,24 +46,25 @@ def prepare_dataset(source, target, voxel_size):
 def execute_global_registration(source_down, target_down, source_fpfh,
                                 target_fpfh, voxel_size):
     distance_threshold = voxel_size * 5
-    print(":: RANSAC registration.")
-    print("   Since the downsampling voxel size is %.3f," % voxel_size)
-    print("   we use a liberal distance threshold %.3f." % distance_threshold)
-    
+    # print(":: RANSAC registration.")
+    # print("   Since the downsampling voxel size is %.3f," % voxel_size)
+    # print("   we use a liberal distance threshold %.3f." % distance_threshold)
+
     result = o3d.registration.registration_ransac_based_on_feature_matching(
         source_down, target_down, source_fpfh, target_fpfh, distance_threshold,
         o3d.registration.TransformationEstimationPointToPoint(False), 4, [
             o3d.registration.CorrespondenceCheckerBasedOnEdgeLength(0.9),
-            o3d.registration.CorrespondenceCheckerBasedOnDistance(distance_threshold)
+            o3d.registration.CorrespondenceCheckerBasedOnDistance(
+                distance_threshold)
         ], o3d.registration.RANSACConvergenceCriteria(4000000, 500))
     return result
 
 
 def refine_registration(source, target, source_fpfh, target_fpfh, voxel_size):
     distance_threshold = voxel_size * 0.4
-    print(":: Point-to-plane ICP registration is applied on original point")
-    print("   clouds to refine the alignment. This time we use a strict")
-    print("   distance threshold %.3f." % distance_threshold)
+    # print(":: Point-to-plane ICP registration is applied on original point")
+    # print("   clouds to refine the alignment. This time we use a strict")
+    # print("   distance threshold %.3f." % distance_threshold)
     result = o3d.registration.registration_icp(
         source, target, distance_threshold, result_ransac.transformation,
         o3d.registration.TransformationEstimationPointToPlane())
@@ -73,20 +74,20 @@ def refine_registration(source, target, source_fpfh, target_fpfh, voxel_size):
 def global_registration(source, target, voxel_size):
     global result_ransac
     source_fpfh, target_fpfh = prepare_dataset(source, target, voxel_size)
-    print(source_fpfh, target_fpfh)
     result_ransac = execute_global_registration(source, target,
                                                 source_fpfh, target_fpfh,
                                                 voxel_size)
     result_icp = refine_registration(
         source, target, source_fpfh, target_fpfh, voxel_size)
-    draw_registration_result(source, target, result_icp.transformation)
-    print("\n")
+    # draw_registration_result(source, target, result_icp.transformation)
 
     return result_icp
 
 
 print("--------------------------------------------")
 print("Started Registration", '\n')
+
+# Reading scans from json files
 
 base_folder = os.path.split(folder)[0]
 json_dirs = []
@@ -112,22 +113,36 @@ for file in json_dirs:
     scans[file_name] = points
 
 
-voxel_size = 15
+# Putting the best scan as reference and aligning everything on top of this
 
-# the scan with most number of points will be target
-# and the rest (sources) will register on top of this.
 
+# Sorting scans by the number of scanned elements
 dates = list(scans.keys())
 len_scans = []
 for i in dates:
     len_scans.append(len(scans[i]))
 dates = [x for _, x in sorted(zip(len_scans, dates))]
 
+voxel_size = 15
 target = to_o3d_cloud(scans[dates[-1]])
-
 global_reg = {}
+pcd = {dates[-1]: target}
+
+# Finding transformation matrices to register all pointclouds on top of target
 for i in range(len(dates)-1):
     source = to_o3d_cloud(scans[dates[i]])
+    pcd[dates[i]] = source
     icp_res = global_registration(source, target, voxel_size)
     global_reg[dates[i]] = icp_res.transformation
+global_reg[dates[i]] = icp_res.transformation
+global_reg[dates[len(dates)-1]] = np.identity(4)
 
+aligned = []
+for key in global_reg:
+    global_reg[key] = np.asarray(global_reg[key], dtype=np.int16)
+    aligned.append(pcd[key].transform(global_reg[key]))
+    
+
+# Visualizing transformation
+print(aligned)
+o3d.visualization.draw_geometries(aligned, width=1300, height=800)
