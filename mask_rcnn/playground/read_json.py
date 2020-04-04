52adb6e874130a0ee062184327e4ddda26b17d90
aldo-sollazzo <aldo@noumena.io>
2020-04-05 01:46:23 +0200

    json check update

--- mask_rcnn/playground/read_json.py
+++ mask_rcnn/playground/read_json.py
@@ -1,28 +1,28 @@
 import cv2
 import numpy as np
-import glob
+import imutils
 import json
+import os
 
 #PARAMETERS
-path_img = '/Users/aldo/Desktop/untitled/01-training/images/0.JPG'
-folder_imgs = '/Users/aldo/Desktop/untitled/01-training/images/'
+path = '/Users/aldo/Desktop/untitled/01-training/images/'
 json_path = '/Users/aldo/Desktop/git_romi/sky_crop/mask_rcnn/playground/via_region_data.json'
-filenames = [img for img in glob.glob("/Users/aldo/Desktop/untitled/01-training/images/*.JPG")]
-
-org = filenames.sort()
-print(org)
-
-# read image
-img = cv2.imread(path_img,1)
-height,width,depth = img.shape
 
 #open file
 with open(json_path) as json_file:
     data = json.load(json_file)
 
     for item in data:
+        img_name = data[item]['filename']
+        img_path = os.path.join(path,img_name)
+        print(img_path)
+
+        # read image
+        img = cv2.imread(img_path, 1)
+        height, width, depth = img.shape
+
+        # create mask
         mask = np.zeros((height, width), dtype="uint8")
-        print(data[item]['filename'])
 
         # draw polylines
         points_x = data[item]['regions'][0]['shape_attributes']['all_points_x']
@@ -30,11 +30,9 @@ with open(json_path) as json_file:
         ptsList = np.column_stack((points_x, points_y))
         cv2.polylines(mask, [ptsList], True, (255, 2550, 255), 10)
 
-    for filename in os.listdir(folder_imgs):
-        print(filename)
-        cv2.imread()
+        # visualize image
+        mask_resized = imutils.resize(mask, width=800)
+        cv2.imshow('mask-polyline',mask_resized)
+        cv2.waitKey(0)
 
-
-        # mask_resized = imutils.resize(mask, width=800)
-        # cv2.imshow('mask-polyline',mask_resized)
-        # cv2.waitKey(0)
+# https://stackoverflow.com/questions/25074488/how-to-mask-an-image-using-numpy-opencv
