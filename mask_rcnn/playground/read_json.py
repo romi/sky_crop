3a887a7f08a641c84648f7f245bff58fc55cd7a2
aldo-sollazzo <aldo@noumena.io>
2020-04-05 02:12:31 +0200

    json check update

--- mask_rcnn/playground/read_json.py
+++ mask_rcnn/playground/read_json.py
@@ -28,11 +28,14 @@ with open(json_path) as json_file:
         points_x = data[item]['regions'][0]['shape_attributes']['all_points_x']
         points_y = data[item]['regions'][0]['shape_attributes']['all_points_y']
         ptsList = np.column_stack((points_x, points_y))
-        cv2.polylines(mask, [ptsList], True, (255, 2550, 255), 10)
+        cv2.polylines(img, [ptsList], True, (255, 2550, 255), 10)
 
         # visualize image
-        mask_resized = imutils.resize(mask, width=800)
+        mask_resized = imutils.resize(img, width=800)
         cv2.imshow('mask-polyline',mask_resized)
         cv2.waitKey(0)
+
+        # # mask on image
+        # masked_data = cv2.bitwise_and(img, img, mask=mask_resized)
 
 # https://stackoverflow.com/questions/25074488/how-to-mask-an-image-using-numpy-opencv
