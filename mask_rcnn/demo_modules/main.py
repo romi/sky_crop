24f1d5f16b9d9f9402bba713beaf542c38ec7ef4
coroush <soroush.garivani@iaac.net>
2020-04-20 20:37:08 +0200

    dataframe

--- mask_rcnn/demo_modules/main.py
+++ mask_rcnn/demo_modules/main.py
@@ -7,6 +7,7 @@ import cv2
 import imutils
 import copy
 import numpy as np
+import pandas as pd
 from PIL import Image
 
 
@@ -55,34 +56,60 @@ if __name__ == "__main__":
         if is_date(dir):
             dates.append(dir)
     dates.sort()
+    data = {}
+    tt = 0
+
+    # ------------------ for dataframe -------------------------
+    # scans = {}
+    # plants = []
+    # names = []
+    # for i in range(20):
+    #     plants.append('{}:area'.format(i))
+    #     names.append('p-{}'.format(i))
+    # scans['plant'] = names
+    # for date in dates:
+    #     scans[date] = plants
+    # dataframe = pd.DataFrame(data=scans, columns=scans.keys())
+    # print(dataframe)
+    
     for date in dates:
-        a = Scan(date)
-        # angle, diagonal = a.pca_angle(a.dir)
-        # aligned = a.align(a.dir, angle, diagonal)
-        aligned = cv2.imread(a.dir)
-        height, width = aligned.shape[:2]
-        cropped = a.crop(aligned)
-        plants_cor = np.zeros((1, 2), dtype=np.int32)
-        total_detected = copy.copy(aligned)
-        t = 0
-        for i in cropped:
-            # only runs the code for first cropped image, remove this "if" part to get the full detected image
-            if t < 1: 
-                cell_x = int(i.split(',')[0])
-                cell_y = int(i.split(',')[1])
-                img = cropped[i]
-                img, cell_points = a.detect(img)
-                x_offset = img.shape[1] * cell_x
-                y_offset = img.shape[0] * cell_y
-                total_detected[y_offset:y_offset+img.shape[0], x_offset:x_offset+img.shape[1]] = img
-                pts = a.assemble_points(cell_points, cell_x, cell_y)
-                plants_cor = np.append(plants_cor, pts, axis=0)
-            t += 1
-        plants_cor = plants_cor[1:, :]
-        total_detected = imutils.resize(total_detected, width=1200)
-        a.detected_crop = total_detected
-        cv2.imshow('total_detected', a.detected_crop)
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
+        if tt < 1:
+            tt += 1
+            plants = []
+            for i in range(20):
+                plants.append(i)
+            a = Scan(date)
+            # angle, diagonal = a.pca_angle(a.dir)
+            # aligned = a.align(a.dir, angle, diagonal)
+            aligned = cv2.imread(a.dir)
+            height, width = aligned.shape[:2]
+            cropped = a.crop(aligned)
+            plants_cor = np.zeros((1, 2), dtype=np.int32)
+            total_detected = copy.copy(aligned)
+            t = 0
+            for i in cropped:
+                # only runs the code for first cropped image, remove this "if" part to get the full detected image
+                if t < 2:
+                    t += 1
+                    cell_x = int(i.split(',')[0])
+                    cell_y = int(i.split(',')[1])
+                    img = cropped[i]
+                    img, cell_points = a.detect(img)
+                    x_offset = img.shape[1] * cell_x
+                    y_offset = img.shape[0] * cell_y
+                    total_detected[y_offset:y_offset+img.shape[0],
+                                   x_offset:x_offset+img.shape[1]] = img
+                    pts = a.assemble_points(cell_points, cell_x, cell_y)
+                    plants_cor = np.append(plants_cor, pts, axis=0)
+            plants_cor = plants_cor[1:, :]
+            total_detected = imutils.resize(total_detected, width=1200)
+            a.detected_crop = total_detected
+            cv2.imshow('total_detected', a.detected_crop)
+            cv2.waitKey(0)
+            cv2.destroyAllWindows()
+            data[a.name] = [plants_cor]
+    dataframe = pd.DataFrame(data, columns=dates)
+    print (dataframe)
+    
 else:
     pass
